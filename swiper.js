5076a52d591673ee0b67d984f877bda3a376b5ed
najmeh-hd <havedanlou3@gmail.com>
2025-05-21 15:07:23 +0330

    text swiper

---
+++ swiper.js
@@ -0,0 +1,13 @@
+import Swiper from "swiper";
+import "swiper/css";
+import Swiper from "swiper";
+import { Pagination } from "swiper/modules";
+import "swiper/css/pagination";
+const swiper = new Swiper(".swiper", {
+  modules: [Pagination],
+  direction: "vertical",
+  loop: true,
+  pagination: {
+  el: ".swiper-pagination",
+  },
+});
