20e270dbe377780a238009ff6475ecd695474228
najmeh-hd <havedanlou3@gmail.com>
2025-05-23 13:31:57 +0330

    timer part

--- main.js
+++ main.js
@@ -32,5 +32,63 @@ document.addEventListener("DOMContentLoaded", function () {
 //   modules: [Pagination],
 //   pagination: {
 //     el: ".swiper-pagination",
+//     clickable: true,
 //   },
 // });
+
+const targetTimestamp = 1751121000000;
+function toPersianDigits(str) {
+  return str.toString().replace(/\d/g, d => "۰۱۲۳۴۵۶۷۸۹"[d]);
+}
+function updateCountdown() {
+  const now = new Date();
+  const distance = targetTimestamp - now;
+
+  if (distance <= 0) {
+    document.getElementById("countdown").innerText = "زمان تخفیف به پایان رسید!";
+    return;
+  }
+  const days = `${Math.floor(distance / (1000 * 60 * 60 * 24))
+    .toString()
+    .padStart(2, "0")}`;
+  document.getElementById("days").innerText = toPersianDigits(days);
+
+  const hours = `${Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60))
+    .toString()
+    .padStart(2, "0")}`;
+  document.getElementById("hours").innerText = toPersianDigits(hours);
+
+  const minutes = `${Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60))
+    .toString()
+    .padStart(2, "0")} : `;
+  document.getElementById("minutes").innerText = toPersianDigits(minutes);
+
+  const seconds = `${Math.floor((distance % (1000 * 60)) / 1000)
+    .toString()
+    .padStart(2, "0")} : `;
+  document.getElementById("seconds").innerText = toPersianDigits(seconds);
+
+
+  // const text = `${days.toString().padStart(2, "0")}  ${hours.toString().padStart(2, "0")} : ${minutes
+  //   .toString()
+  //   .padStart(2, "0")} : ${seconds.toString().padStart(2, "0")}`;
+  // document.getElementById("countdown").innerText = toPersianDigits(text);
+}
+
+setInterval(updateCountdown, 1000);
+updateCountdown();
+
+function submitForm() {
+  const phone = document.getElementById("phone").value.trim();
+  const name = document.getElementById("name").value.trim();
+
+  if (!phone || !name) {
+    alert("لطفا نام و شماره تماس را وارد کنید.");
+    return;
+  }
+
+  // نمایش کد تخفیف
+  alert(`${name} عزیز، کد تخفیف شما: OFF50`);
+
+  // می‌تونی اینجا اطلاعات رو به سرور هم بفرستی
+}
